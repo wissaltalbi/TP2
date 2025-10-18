359aff26df0dc2166b1df8c2fdc856360754f587
wissaltalbi <wissaltalbi65@gmail.com>
2025-10-18 23:34:13 +0100

    Test de la fonctionnalité login par Client2

--- Login.java
+++ Login.java
@@ -1,5 +1,7 @@
 public class Login {
     public static void main(String[] args) {
         System.out.println("Feature Login implemented by Client1");
+        System.out.println("Login feature tested by Client2");
+
     }
 }
